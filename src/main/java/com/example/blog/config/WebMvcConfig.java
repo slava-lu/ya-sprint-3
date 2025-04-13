dde1fff9a94d73511af2397308abfca06d0b5db4
slavalu <slavalu74@gmail.com>
2025-04-13 15:02:46 +0200

    initial commit

---
+++ src/main/java/com/example/blog/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.blog.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.example.blog.controller")
+public class WebMvcConfig  {
+}
