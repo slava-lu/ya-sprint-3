29a3605dc62920cf9bca5f9b444144e522abd141
slavalu <slavalu74@gmail.com>
2025-06-01 14:10:25 +0200

    adding images

--- src/main/java/com/example/blog/config/WebMvcConfig.java
+++ src/main/java/com/example/blog/config/WebMvcConfig.java
@@ -6,14 +6,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.example.blog")
 @PropertySource("classpath:application.properties")
-public class WebMvcConfig  {
+public class WebMvcConfig implements WebMvcConfigurer {
+
     @Bean
     public StandardServletMultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/images/**")
+                .addResourceLocations("/images/");
     }
 }
